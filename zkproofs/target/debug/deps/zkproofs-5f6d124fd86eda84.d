acd273641c299866b520beb371e4fd25987770be
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2026-04-01 21:56:23 +0100

    feat: Knowledge Test

--- zkproofs/target/debug/deps/zkproofs-5f6d124fd86eda84.d
+++ zkproofs/target/debug/deps/zkproofs-5f6d124fd86eda84.d
@@ -1,5 +1,7 @@
-/Users/anu/ZK-Implementation/zkproofs/target/debug/deps/zkproofs-5f6d124fd86eda84.d: src/main.rs
+/Users/anu/ZK-Implementation/zkproofs/target/debug/deps/zkproofs-5f6d124fd86eda84.d: src/main.rs src/new/mod.rs src/new/generic.rs
 
-/Users/anu/ZK-Implementation/zkproofs/target/debug/deps/libzkproofs-5f6d124fd86eda84.rmeta: src/main.rs
+/Users/anu/ZK-Implementation/zkproofs/target/debug/deps/libzkproofs-5f6d124fd86eda84.rmeta: src/main.rs src/new/mod.rs src/new/generic.rs
 
 src/main.rs:
+src/new/mod.rs:
+src/new/generic.rs:
